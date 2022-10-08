ce620d9f61e63da28a6ad009560a0a5744071b59
Andew Maslenikov <wtfthefirefox@gmail.com>
2022-10-09 01:33:32 +0300

    added readme and first ver of conver(only any to mp4)

---
+++ README.md
@@ -0,0 +1,6 @@
+# Video converter from any type of video to mp4
+
+---
+Based on
++ [ffmpeg](https://github.com/kkroening/ffmpeg-python)
+---
---
+++ test.py
@@ -0,0 +1,19 @@
+from os import listdir
+from os.path import isfile, join
+import ffmpeg
+
+def find_nth(str, symbol, n):
+    start = str.find(symbol)
+    while (start >= 0 and n > 1):
+        start = str.find(symbol, start+len(symbol))
+        n -= 1
+    return start
+
+arr_of_files = [f for f in listdir("./samples") if (isfile(join("./samples", f)) and f.find('.DS_Store') == -1)] # get files from input
+arr_of_files = map(lambda f: './samples/' + f, arr_of_files);
+
+for file in arr_of_files:
+  (ffmpeg
+        .input(file)
+        .output('./result/' + file[find_nth(file, '/', 2) + 1:file.rfind('.')] + '.mp4')
+        .run())
